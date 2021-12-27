4d0b58bf9a8add7189a416900428744f42b02ae3
konstantin <makarovestates@gmail.com>
2021-12-26 20:04:46 -0600

    test attempt

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts[i]){
+    //for (let i = 0; i < gifts.length; i++){
+        //console.log(`Wrapped ${gifts[i]} and added a bow!`); 
+       
+    //}
+
+    //return gifts; 
+
+//}
+
+//wrapGifts(gifts); 
+
+
+const names = (["Guadalupe", "Ollie", "Aki"]);
+
+const message = []; 
+
+function writeCards(names, message){
+    for (let i=0; i < names.length; i++){
+        return `Thank you, ${names}, for the wonderful surprise gift!`;
+        
+    }
+}
+
+function countDown(number){
+    let 
+}
